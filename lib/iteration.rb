81c4a19f526bbf31fc9d299d8ada49d27bc5ba28
Kevin Clark <kevin.clark93@gmail.com>
2020-02-02 02:52:47 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -42,7 +42,7 @@ def total_even_pairs(src)
   # this!
   total = 0
   array_index = 0
-  if src[array_index][0] % 2 == 0 &&  src[array_index][1] % 2 == 0
+  if src[array_index][0] % 2 == 0 && src[array_index][1] % 2 == 0
     new_array.push(src[array_index])
     array_index += 1
   end
